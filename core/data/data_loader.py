e21eeef49b081271a2f607baaca759729e0e28fb
Steven Zaldivar <zh15002@ues.edu.sv>
2020-04-21 00:20:59 -0600

    Manejo de datasets
    ahora se utiliza h5py para cargar los datasets

---
+++ core/data/data_loader.py
@@ -0,0 +1,19 @@
+import h5py
+import numpy as np
+
+def load_data(train_dataset_path, test_dataset_path):
+    train_dataset = h5py.File(train_dataset_path,"r")
+    train_set_x = np.array(train_dataset["train_set_x"][:])
+    train_set_y = np.array(train_dataset["train_set_y"][:])
+    test_dataset = h5py.File(test_dataset_path,"r")
+    test_set_x = np.array(test_dataset["test_set_x"][:])
+    test_set_y = np.array(test_dataset["test_set_x"][:])
+
+    train_set_y = train_set_y.reshape(1, train_set_y.shape)
+    test_set_y = test_set_y.reshape(1, test_set_y.shape)
+
+    return train_set_x, train_set_y, test_set_x, test_set_y
+
+def image_to_column_vector(image: np.ndarray):
+    column_vector_image = image.reshape(image.shape[0], -1).T
+    return column_vector_image
